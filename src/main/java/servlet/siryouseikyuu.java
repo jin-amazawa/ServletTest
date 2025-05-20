97dff96bf6095ea3124ff25e9329623882e3ab27
jin-Amazawa <yuzeren8@gmail.com>
2025-05-20 14:54:59 +0900

    ServletTestの課題を提出しました

---
+++ src/main/java/servlet/siryouseikyuu.java
@@ -0,0 +1,42 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/servlet/siryouseikyuu")
+public class siryouseikyuu extends HttpServlet{
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		
+		String name = request.getParameter("name");
+		String company = request.getParameter("company");
+		String email = request.getParameter("email");
+		String contact = request.getParameter("contact");
+		
+		String[] merumaga_checked = request.getParameterValues("merumagasyurui");
+		
+		String YesOrNo = request.getParameter("hopeYesOrNo"); 
+		
+		
+		out.println("<p>氏名：" + name + "</p>");
+		out.println("<p>会社：" + company + "</p>");
+		
+		
+		if("Yes".equals(YesOrNo)) {
+			out.println("この度は資料請求いただきありがとうございます。以下のリンクから資料ダウンロードが可能です。");
+		}else {
+			out.println("資料請求を希望していません");
+		}
+		
+		out.println("<p><a href = '#'>ダミーリンク</a></p>");
+		
+	}
+}
---
+++ src/main/webapp/contact.jsp
@@ -0,0 +1,31 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+
+
+	<form action = "/ServletTest/servlet/siryouseikyuu" method = "POST">
+	
+	<p>氏名: <input type = "text" name = "name" required></p>
+	<p>会社: <input type = "text" name = "company" ></p>
+	<p>メールアドレス: <input type = "text" name = "email" required></p>
+	<p>お問い合わせ内容: <br><textarea rows="6" cols="50" name = "contact" required></textarea></p>
+	<p>メルマガ種類:</p>
+	<p><input type = "checkbox" name = "merumagasyurui">総合案内</p>
+	<p><input type = "checkbox" name = "merumagasyurui">セミナー案内</p>
+	<p><input type = "checkbox" name = "merumagasyurui">求人採用情報</p>
+	<p>資料請求希望</p>
+	<p>Yes<input type = "radio" name = "hopeYesOrNo" value = "Yes"></p>
+	<p>No<input type = "radio" name = "hopeYesOrNo" value = "No"></p>
+	<input type = "submit" value = "送信">
+	
+	</form>
+
+
+</body>
+</html>
